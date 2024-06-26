6a0d98ed7fdb01a867dccb4fb55ca5ffd9232f1d
Brian Behm <bbehm@omnitech-inc.com>
2024-06-26 09:36:47 -0500

    Add OpenTelemetry libraries

--- HandsOnOpenTelemetry.Api/Program.cs
+++ HandsOnOpenTelemetry.Api/Program.cs
@@ -1,9 +1,19 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService("dotnet-frontend"))
+    .WithTracing(tpb =>
+        tpb
+            .AddAspNetCoreInstrumentation()
+            .AddConsoleExporter());
 
 var app = builder.Build();
 
